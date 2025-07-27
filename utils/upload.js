64d8d5bf30533573431065f32df039f05a7e0e70
GoDavai01 <info@godavaii.com>
2025-07-27 18:35:18 +0530

    feat and update

--- utils/upload.js
+++ utils/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const s3 = require('./s3-setup');
 
-const allowedTypes = ['image/jpeg', 'image/png', 'application/pdf'];
+const allowedTypes = ['image/jpeg', 'image/png', 'image/webp', 'application/pdf'];
 const upload = multer({
   storage: multerS3({
     s3: s3,
